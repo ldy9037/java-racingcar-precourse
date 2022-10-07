3d46a5dcf1d3dac7c1c9d0873877b91b7dd5b9b1
emadam <ldy9037@naver.com>
2022-10-07 15:10:04 +0900

    feat(view): 에러가 발생하면 그 부분 부터 다시 입력을 받는다
    - 공백 라인을 추가하는 printNewLine 메서드 구현
    - 잘못 입력하면 에러 메세지 출력 후 입력을 재시도하는 기능 구현
    
    https://emadam.notion.site/feat-6916bdd9efde4ac9b147a59feecd2145

--- src/main/java/racingcar/view/MainView.java
+++ src/main/java/racingcar/view/MainView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import java.util.Objects;
+
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.MainController;
 import racingcar.domain.Cars;
@@ -7,23 +9,60 @@ import racingcar.domain.RoundCount;
 
 public class MainView {
     public static void carNameForm(MainController mainController) {
-        printCarNameFormMessage();
-
-        Cars.from(Console.readLine());
+        Cars cars = null;
+        
+        while (Objects.isNull(cars)) {
+            printCarNameFormMessage();
+            cars = inputCarNames();
+            printNewLine();
+        }
+         
         mainController.requestRoundCountForm();
     }    
 
     public static void roundCountForm(MainController mainController) {
-        roundCountFormMessage();
+        RoundCount roundCount = null;
 
-        RoundCount roundCount = new RoundCount(Console.readLine());
+        while (Objects.isNull(roundCount)) {
+            printRoundCountFormMessage();
+            roundCount = inputRoundCount();
+            printNewLine();
+        }
     }
 
-    private static void roundCountFormMessage() {
+    private static Cars inputCarNames() {
+        Cars result = null;
+
+        try {
+            result = Cars.from(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
+    private static RoundCount inputRoundCount() {
+        RoundCount result = null;
+
+        try {
+            result = new RoundCount(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
+    private static void printRoundCountFormMessage() {
         System.out.println("시도할 횟수는 몇회인가요?");
     }
 
     private static void printCarNameFormMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    private static void printNewLine() {
+        System.out.println();
     }
 }
